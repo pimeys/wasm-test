38e89200052d53c5bcd979bf8beb107fe6bdca4b
Julius de Bruijn <julius+github@nauk.io>
2023-05-03 19:25:50 +0200

    First minimal sample

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
-import { greet } from './dist/wasm_test';
+import { Grafbase } from './dist/wasm_test';
 
-console.log(greet("World"));
+// This we export
+const g = new Grafbase();
+
+const user = g.model("User", {
+  name: g.string(),
+  age: g.int(),
+  // can we make this typesafe, so we have to pass a string?
+  // lol: 3,
+});
+
+console.log(user);
